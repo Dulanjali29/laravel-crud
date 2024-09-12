ae9ba95e9353ac1b52d10426a3256bbaa9d0589f
Dulanjali29 <dulanjiariyarathna@gmail.com>
2024-09-12 16:42:23 +0530

    add boostrap and route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('products.index');
 });
+Route::get('/products',[ProductsController::class,'index']);
